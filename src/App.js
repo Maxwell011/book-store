44ec2a8757cee98aa5ae99c944092135d91b843e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-11-03 15:57:09 +0100

    Added book page and display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './pages/Categories';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Books />} />
+          <Route exact path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
