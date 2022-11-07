b2643b5aaf08a13a14fba9f5dfa8d5b773cb0c49
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-11-07 19:05:38 +0100

    Book actions and reducer

---
+++ src/redux/books/books.js
@@ -0,0 +1,27 @@
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+const initialState = [];
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (book) => ({
+  type: REMOVE_BOOK,
+  book,
+});
+
+function bookReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return [...state.filter((each) => each.id !== action.payload.id)];
+    default:
+      return state;
+  }
+}
+
+export default bookReducer;
