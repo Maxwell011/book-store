b574829abd4a1775a361c789254dcfd6b4b04cee
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-11-10 15:21:38 +0100

    Updated file to fetch Api and thunk

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './Books.module.css';
 import AddBook from './AddBook';
 import Book from './Book';
+import { getBooks } from '../redux/books/books';
 
 function Books() {
   // eslint-disable-next-line no-unused-vars
+  const dispatch = useDispatch();
+
   const books = useSelector((state) => state.booksReducer);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <div className={styles.container}>
       <div className={styles.bookList}>
         <ul className={styles.item}>
-          {books && books.map((book) => <Book key={book.id} book={book} />)}
+          {books?.map((book) => <Book key={book.id} book={book} />)}
         </ul>
       </div>
       <AddBook />
